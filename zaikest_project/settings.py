6547fe92f3af4a6c0bc032572db8b37dca453f97
Sardarmani <muhammadmuneebtariq07@gmail.com>
2026-01-13 21:12:10 +0500

    Added temporay user

--- zaikest_project/settings.py
+++ zaikest_project/settings.py
@@ -144,3 +144,28 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CART_SESSION_ID = 'cart'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+import os
+
+if os.environ.get("CREATE_SUPERUSER") == "1":
+    try:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        print("SSSSSSSSSSSSSSSSSSSSSSSS")
+        username = os.environ.get("SU_NAME", "admin")
+        email = os.environ.get("SU_EMAIL", "admin@example.com")
+        password = os.environ.get("SU_PASSWORD", "admin123")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            print("✅ Superuser created successfully")
+        else:
+            print("ℹ️ Superuser already exists")
+
+    except Exception as e:
+        print("❌ Error creating superuser:", e)
