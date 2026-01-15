f2b662ee7299438b399b50b4dab3b4581e93f9fc
Sardarmani <muhammadmuneebtariq07@gmail.com>
2026-01-15 23:51:32 +0500

    Added Order View in DB...

--- zaikest_project/settings.py
+++ zaikest_project/settings.py
@@ -41,10 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "store",
     "pages",
     "store.apps.StoreConfig",
-
 ]
 
 MIDDLEWARE = [
